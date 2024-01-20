81e766e607a8cc57a99551dea5250c1719c9201f
bartek <taras_basket@wp.pl>
2024-01-20 14:26:36 +0100

    using withLatestFrom in effect

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,19 +1,25 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './counter.actions';
-import { tap } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectCount } from './counter.selectors';
 
 export class CounterEffects {
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
         ofType(increment, decrement),
-        tap((action) => {
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
           console.log(action);
-          localStorage.setItem('count', action.value.toString());
+          localStorage.setItem('count', counter.toString());
         })
       ),
     { dispatch: false }
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store<{ counter: number }>
+  ) {}
 }
